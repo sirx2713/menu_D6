4b5c0bf70b996474523a046dce4da7f8e2ab1eca
Sirx <bradx2713@gmail.com>
2024-02-29 05:17:44 +0200

    Initial Commit

---
+++ menu_D6/menu_D6App.swift
@@ -0,0 +1,17 @@
+//
+//  menu_D6App.swift
+//  menu_D6
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct menu_D6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
